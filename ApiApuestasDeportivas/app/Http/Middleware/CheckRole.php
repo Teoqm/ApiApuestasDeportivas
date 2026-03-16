cdb7d753763bbd6ac8bbbec460f5783f78ed16ef
mateo <mateoq152006@gmail.com>
2026-03-15 22:07:28 -0500

    arrgla un erro con lo roles

---
+++ ApiApuestasDeportivas/app/Http/Middleware/CheckRole.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class CheckRole
+{
+
+    public function handle(Request $request, Closure $next, ...$roles)
+    {
+        if(! $request->user()){
+            return response()->json(['message'=>'No autenticado'], 401);
+        }
+
+        if(! in_array($request->user()->role, $roles)){
+            return response()->json([
+                'message'=> 'No tienes permiso para esta acción, Rol es requerido'
+            ], 403);
+
+        }
+        return $next($request);
+    }
+}
