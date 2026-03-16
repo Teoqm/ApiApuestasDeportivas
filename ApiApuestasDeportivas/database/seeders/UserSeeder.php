fdd18e08842a8e679be27e2f6e26797ab3f41641
mateo <mateoq152006@gmail.com>
2026-03-15 20:06:34 -0500

    se crea seeder user para que la base de dtos tenga informacion

--- ApiApuestasDeportivas/database/seeders/UserSeeder.php
+++ ApiApuestasDeportivas/database/seeders/UserSeeder.php
@@ -2,16 +2,28 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
-    public function run(): void
+    public function run()
     {
-        //
+        User::create([
+            'name' => 'Administrador',
+            'email' => 'admin@api.com',
+            'password' => Hash::make('password'),
+            'role' => User::ROLE_ADMIN,
+            'saldo' => 1000000
+        ]);
+
+        User::create([
+            'name' => 'Usuario Prueba',
+            'email' => 'usuario@api.com',
+            'password' => Hash::make('password'),
+            'role' => User::ROLE_USUARIO,
+            'saldo' => 500000
+        ]);
     }
 }
